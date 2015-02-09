67d29e1c0c0bdcebec96147787f6f241671b5e24
micul <micul@micul-T60>
2015-02-10 00:08:18 +0100

    Added application deployment (at the end of application generation)

--- POCr-POC/src/main/java/com/test/pocr/application/ApplicationGenerator.java
+++ POCr-POC/src/main/java/com/test/pocr/application/ApplicationGenerator.java
@@ -2,7 +2,17 @@ package com.test.pocr.application;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+
+import com.test.pocr.exception.PocrException;
 
 public class ApplicationGenerator {
 
@@ -10,6 +20,9 @@ public class ApplicationGenerator {
 
 	private final ApplicationModel model;
 	private final File outputFolder;
+
+	// TODO get it from the environment variables
+	public static final String M2_HOME = "/usr/local/maven";
 
 	public ApplicationGenerator(final ApplicationModel model) {
 		this.model = model;
@@ -24,6 +37,7 @@ public class ApplicationGenerator {
 	public void generateApplication() throws IOException {
 		recreateFolder();
 		writeArtifacts(model.getArtifacts());
+		deployApplication();
 	}
 
 	private void recreateFolder() {
@@ -40,5 +54,25 @@ public class ApplicationGenerator {
 		for (final IGenerator artifact : artifacts) {
 			artifact.writeInFolder(outputFolder);
 		}
+	}
+
+	private void deployApplication() {
+		final InvocationRequest request = new DefaultInvocationRequest();
+		request.setGoals(Collections
+				.singletonList("com.oracle.weblogic:wls-maven-plugin:deploy"));
+		request.setBaseDirectory(outputFolder);
+
+		final Invoker invoker = new DefaultInvoker();
+		invoker.setMavenHome(new File(M2_HOME));
+
+		try {
+			final InvocationResult result = invoker.execute(request);
+			if (result.getExitCode() != 0) {
+				System.out.println("Naspa");
+			}
+		} catch (final MavenInvocationException e) {
+			throw new PocrException("Problem occured at deployment", e);
+		}
+
 	}
 }
